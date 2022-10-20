818f6468def68293d211710f61b14d4f314b3484
Bitten 2up <seant9140@gmail.com>
2022-10-19 20:33:24 -0500

    s

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,86 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::{WindowCanvas, Texture};
+use sdl2::rect::{Point, Rect};
+// "self" imports the "image" module itself as well as everything else we listed
+use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
+use sdl2::gfx::framerate::FPSManager;
+
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    let (width, height) = canvas.output_size()?;
+
+    // Treat the center of the screen as the (0, 0) coordinate
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    canvas.copy(texture, sprite, screen_rect)?;
+
+    canvas.present();
+
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
+    // stay unused because if we don't have any variable at all then Rust will treat it as a
+    // temporary value and drop it right away!
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("rusted bitten", 800, 600)
+    let window = video_subsystem.window("rusty bitten", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bitten.png")?;
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let position = Point::new(0, 0);
+    // src position in the spritesheet
+    let mut frame = 0;
+    let mut sprite = Rect::new(frame, 0, 32, 32);
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
+                Event::KeyDown { keycode: Some(Keycode::Return), ..} => {
+                    frame+=32;
+                    sprite = Rect::new(0, frame, 32, 32);
+                    if frame==32*3 {
+                        frame=0
+                    }
+                }
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
+
+        // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
